886b4fbe053e1c39bc20d8acf040ae4a6c73f754
Jarek Malec <dzardacz@gmail.com>
2016-09-06 20:36:25 +0200

    Problem 2 solved

--- src/bin/problem2.rs
+++ src/bin/problem2.rs
@@ -1,19 +1,24 @@
 
-
 fn fib_sum(to: i64) -> i64 {
-	let mut prev = 1i64;
-	let mut last = 2i64;
-	let mut acc = 2i64;
+	let mut prev = 1;
+	let mut last = 2;
+	let mut acc = 0;
 	while last < to {
-		if last % 2 == 0 {
+		if (last & 1) == 0 {
 			acc += last;
 		}
-		last = last + prev;
+		let newlast = last + prev;
 		prev = last;
+		last = newlast;
 	}
+	acc
 }
 
 #[test]
 fn test_fib_sum() {
-	assert!(fib_sum(34) == 44);
+	assert!(fib_sum(30) == 10);
+}
+
+fn main() {
+	println!("{}", fib_sum(4_000_000));
 }
